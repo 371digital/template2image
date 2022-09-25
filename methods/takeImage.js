021af4e0f47bba74e48d269e1c769ca77d0f772f
Gron <ahesteaheste0101@email.com>
2022-09-25 04:05:54 +0300

    v1.0.4 - options & delay feature

--- methods/takeImage.js
+++ methods/takeImage.js
@@ -1,7 +1,11 @@
 const puppeteer = require("puppeteer");
 const chromium = require('chrome-aws-lambda');
 
-const takeImage = async (html, elementId = "#snapShotElement") => {
+const delay = async (ms) => {
+    return await new Promise(r => setTimeout(r, ms));
+};
+
+const takeImage = async (html, elementId = "#snapShotElement", options = {}) => {
     const browser = await puppeteer.launch({
         devtools: false, args: [
             ...chromium.args,
@@ -15,10 +19,10 @@ const takeImage = async (html, elementId = "#snapShotElement") => {
             waitUntil: 'networkidle0',
         });
     };
-    
+
     if (html.url) {
         await page.goto(html.url, {
-            waitUntil: 'networkidle2',
+            waitUntil: 'networkidle0',
         });
     };
 
@@ -26,6 +30,8 @@ const takeImage = async (html, elementId = "#snapShotElement") => {
     await page.evaluate(() => document.body.style.background = 'transparent');
     await page.waitForSelector(`#${elementId}`)
     const content = await page.$(`#${elementId}`);
+    
+    if (options.delay) await delay(options.delay);
 
     const imageBuffer = await content.screenshot({ encoding: "base64", omitBackground: true });
     await page?.close();
